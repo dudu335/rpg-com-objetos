b2ca6e4e8281dfbc6d3ae85d9ca61a88a55113bf
isavamp <isadora.7791@colegioprogresso.g12.br>
2026-08-13 09:28:33 -0300

    inicio rpg

--- app.js
+++ app.js
@@ -26,4 +26,31 @@ const mago = new personagem("Gandalfe", 60, 25)
 
 const arqueiro = new personagem("Legolas", 80, 25)
 
-console.log(guerreiro)
+const titã = new personagem("Kryonix", 100, 50)
+
+const anjo = new personagem("Muriel", 110, 15)
+
+const anão = new personagem("zangado", 30, 67)
+
+console.log(guerreiro, "\n",druida,"\n", mago,"\n", arqueiro,"\n", titã, "\n",anjo, "\n",anão)
+
+
+druida.causaDano(guerreiro);
+console.log("\nKode atacou Thorin");
+console.log("Vida do Thorin:", guerreiro.vida);
+
+guerreiro.causaDano(druida);
+console.log("\nThorin atacou Kode");
+console.log("Vida do Kode: ", druida.vida );
+
+druida.causaDano(guerreiro);
+console.log("\nKode atacou Thorin");
+console.log("Vida do Thorin:", guerreiro.vida);
+
+guerreiro.causaDano(druida);
+console.log("\nThorin atacou Kode");
+console.log("Vida do Kode:", druida.vida);
+
+titã.causaDano(anjo);
+console.log("\nKryonix atacou muriel");
+console.log("vida do Muriel", anjo.anjo)
