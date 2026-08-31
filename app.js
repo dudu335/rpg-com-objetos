24aa8421e58e17a7c6c027d7925a5ac6faf5a1fb
marcL2010 <marcia.7986@colegioprogesso.g12.br>
2026-08-31 08:37:36 -0300

    novas regras

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 class personagem{
-    constructor(nome, vida, ataque){
+    constructor(nome, vida, ataque, defesa){
         this.nome = nome,
         this.vida = vida,
-        this.ataque = ataque
+        this.ataque = ataque,
+        this.defesa = defesa
     }
 
+    curar(quantidade){
+    this.vida = this.vida + quantidade
+    if(this.vida > this.vidaMaxima){
+    this.vida = this.vidaMaxima
+}
+    }
     causaDano(personagem){
         personagem.recebeDano(this.ataque)
         
     }
     
     recebeDano(quantidade){
-       this.vida = this.vida - quantidade 
+         let dano = quantidade - this.defesa
+
+     if(quantidade < this.defesa)
+        dano = 0
+     
+       this.vida = this.vida - dano
         
        if(this.vida <=0){
         this.vida = "eliminado"
@@ -25,13 +37,13 @@ console.log(this.nome + " | Vida: " + this.vida)
 }
 
 }
-const druida = new personagem("Kode", 80, 25)
-const guerreiro = new personagem("Thorin", 60, 20)
-const mago = new personagem("Gandalfe", 60, 25)
-const arqueiro = new personagem("Legolas", 80, 25)
-const titã = new personagem("Kryonix", 100, 50)
-const anjo = new personagem("Muriel", 110, 15)
-const anão = new personagem("zangado", 30, 67)
+const druida = new personagem("Kode", 80, 25, 10)
+const guerreiro = new personagem("Thorin", 60, 20, 5)
+const mago = new personagem("Gandalfe", 60, 25,15)
+const arqueiro = new personagem("Legolas", 80, 25, 12)
+const titã = new personagem("Kryonix", 100, 50, 5)
+const anjo = new personagem("Muriel", 110, 15, 20)
+const anão = new personagem("zangado", 30, 67, 10)
 console.log(guerreiro, "\n",druida,"\n", mago,"\n", arqueiro,"\n", titã, "\n",anjo, "\n",anão)
 
 
